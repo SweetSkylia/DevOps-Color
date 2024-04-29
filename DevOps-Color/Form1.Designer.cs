5eed5433613dad0af00f9d656d5ceaeb31e17a96
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-04-29 13:41:35 +0200

    First Commit

--- DevOps-Color/Form1.Designer.cs
+++ DevOps-Color/Form1.Designer.cs
@@ -15,6 +15,7 @@
         {
             if (disposing && (components != null))
             {
+                //salut c'est moi
                 components.Dispose();
             }
             base.Dispose(disposing);
