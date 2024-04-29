b171c2fcc50f7530ecea0a9802f8019d8825852b
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-04-29 14:37:52 +0200

    Redesign partie 1

--- DevOps-Color/Form1.cs
+++ DevOps-Color/Form1.cs
@@ -2,6 +2,10 @@ namespace DevOps_Color
 {
     public partial class Form1 : Form
     {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +26,7 @@ namespace DevOps_Color
             //
         }
 
-        private void btnEnregisterer_Click(object sender, Eventargs e){
+        private void btnEnregisterer_Click(object sender, EventArgs e){
             //Sauvegarde
         }
 
@@ -36,6 +40,25 @@ namespace DevOps_Color
 
         }
 
+        private void pnlTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
 
+        private void pnlTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+        }
+
+        private void pnlTop_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
     }
 }
