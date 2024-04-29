0e94f75932e4920eaeaf8a88a0a665858b15fe04
Florianmath <florian.mathi67@gmail.com>
2024-04-29 14:51:54 +0200

    Style : Ajout des panels de couleur
    Ajout des panels de couleur.

--- DevOps-Color/Form1.cs
+++ DevOps-Color/Form1.cs
@@ -6,5 +6,24 @@ namespace DevOps_Color
         {
             InitializeComponent();
         }
+
+        private Color GenerateRandomColor()
+        {
+            Random random = new Random();
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Création et affichage des panneaux avec les couleurs aléatoires
+            for (int i = 0; i < 5; i++)
+            {
+                Panel panel = new Panel();
+                panel.Size = new Size(100, 1000); // Taille du panneau
+                panel.Location = new Point(50 + i * 120, 50); // Position du panneau
+                panel.BackColor = GenerateRandomColor(); // Couleur aléatoire pour le fond du panneau
+                this.Controls.Add(panel); // Ajout du panneau au formulaire
+            }
+        }
     }
 }
