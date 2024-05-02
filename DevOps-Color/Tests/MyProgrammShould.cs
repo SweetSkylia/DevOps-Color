6d96af8d99c18843c234aa0559cf888c910e6c77
VSanzovo <victor.sanzovo@viacesi.fr>
2024-05-02 11:38:52 +0200

    Structure pour les tests unitaires

---
+++ DevOps-Color/Tests/MyProgrammShould.cs
@@ -0,0 +1,88 @@
+using NUnit.Framework;
+using System;
+using System.Windows.Forms;
+
+namespace DevOps_Color.Tests
+{
+    [TestFixture]
+    public class Form1Tests
+    {
+        private Form1 form;
+
+        [SetUp]
+        public void SetUp()
+        {
+            form = new Form1();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            form.Dispose();
+        }
+
+        [Test]
+        public void BtnGenerer_Click_ShouldGenerateColor()
+        {
+            // Arrange
+            // Simuler un événement de clic sur le bouton
+            var eventArgs = new EventArgs();
+
+            // Act
+            form.btnGenerer_Click(this, eventArgs);
+
+            // Assert
+            // Ajouter les assertions pour vérifier le résultat de l'action
+            // Par exemple, vérifier que la couleur est bien générée
+            // Assert.AreEqual(expectedColor, form.SomePropertyRelatedToColor);
+        }
+
+        [Test]
+        public void BtnEnregistrer_Click_ShouldSaveColor()
+        {
+            // Arrange
+            var eventArgs = new EventArgs();
+
+            // Act
+            form.btnEnregistrer_Click(this, eventArgs);
+
+            // Assert
+            // Ajouter les assertions pour vérifier le résultat de l'action
+            // Par exemple, vérifier que la couleur est bien enregistrée
+            // Assert.IsTrue(form.ColorSavedSuccessfully);
+        }
+
+        [Test]
+        public void BtnFermer_Click_ShouldCloseForm()
+        {
+            // Arrange
+            var eventArgs = new EventArgs();
+
+            // Act
+            form.btnQuitter_Click(this, eventArgs);
+
+            // Assert
+            // Vérifiez que le formulaire est fermé
+            Assert.IsTrue(form.IsDisposed);
+        }
+
+        [Test]
+        public void LblTitre_Click_ShouldHandleTitleClick()
+        {
+            // Arrange
+            // Simuler un événement de clic sur le label
+            var eventArgs = new EventArgs();
+
+            // Act
+            form.lblTitre_Click(this, eventArgs);
+
+            // Assert
+            // Ajouter les assertions pour vérifier le résultat de l'action
+            // Par exemple, vérifier que l'interface utilisateur a réagi comme prévu
+        }
+
+        // Continuez à écrire des tests pour d'autres fonctionnalités de Form1
+        // ...
+
+    }
+}
